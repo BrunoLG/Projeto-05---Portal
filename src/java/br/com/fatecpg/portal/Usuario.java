90db3d83ac0f25bc25d549058b47f2d233b47011
BrunoLG <bruno_lg1998@hotmail.com>
2018-12-03 12:49:56 -0200

    Implementação dos métodos da classe Usuario

--- src/java/br/com/fatecpg/portal/Usuario.java
+++ src/java/br/com/fatecpg/portal/Usuario.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class Usuario {
     private long cod;
     private String usuario;
-    private String senha;
+    private long senha;
     private String nome;
     private String curso;
     private String permissao;
 
-    public Usuario(long cod, String usuario, String senha, String nome, String curso, String permissao) {
+    public Usuario(long cod, String usuario, long senha, String nome, String curso, String permissao) {
         this.cod = cod;
         this.usuario = usuario;
         this.senha = senha;
@@ -18,7 +18,7 @@ public class Usuario {
         this.curso = curso;
         this.permissao = permissao;
     }
-
+    
     public long getCod() {
         return cod;
     }
@@ -35,11 +35,11 @@ public class Usuario {
         this.usuario = usuario;
     }
 
-    public String getSenha() {
+    public long getSenha() {
         return senha;
     }
 
-    public void setSenha(String senha) {
+    public void setSenha(long senha) {
         this.senha = senha;
     }
 
@@ -66,10 +66,14 @@ public class Usuario {
     public void setPermissao(String permissao) {
         this.permissao = permissao;
     }
-    
+
     public static Usuario getUsuario(String login, String senha) throws Exception {
-        
-        String SQL = "SELECT * FROM TB_USUARIO WHERE NM_LOGIN = ? AND NM_SENHA = ?";
+        String SQL = "SELECT CD_USUARIO, NM_USUARIO, NM_LOGIN, CD_SENHA," +
+                        "(SELECT C.NM_CURSO FROM TB_CURSO C" +
+                            "WHERE U.CD_CURSO = C.CD_CURSO)," +
+                        "(SELECT P.NM_PERMISSAO FROM TB_PERMISSAO P" +
+                            "WHERE U.CD_PERMISSAO = P.CD_PERMISSAO)" +
+                    "FROM TB_USUARIO U WHERE NM_USUARIO = ? AND CD_SENHA = ?";
         Object parameters[] = {login, senha.hashCode()};
         
         ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, parameters);
@@ -80,7 +84,7 @@ public class Usuario {
             Usuario u = new Usuario(
                     (long) row[0], 
                     (String) row[1], 
-                    (String) row[2], 
+                    (long) row[2], 
                     (String) row[3], 
                     (String) row[4],
                     (String) row[5]);
@@ -88,8 +92,13 @@ public class Usuario {
         }
     }
     
-    public static ArrayList<Usuario> getUsers() throws Exception {
-        String SQL = "SELECT * FROM USERS";
+    public static ArrayList<Usuario> getUsuarios() throws Exception {
+        String SQL = "SELECT CD_USUARIO, NM_USUARIO, NM_LOGIN, CD_SENHA," +
+                        "(SELECT C.NM_CURSO FROM TB_CURSO C" +
+                            "WHERE U.CD_CURSO = C.CD_CURSO)," +
+                        "(SELECT P.NM_PERMISSAO FROM TB_PERMISSAO P" +
+                            "WHERE U.CD_PERMISSAO = P.CD_PERMISSAO)" +
+                    "FROM TB_USUARIO U";
         ArrayList<Usuario> usuarios = new ArrayList<>();
         ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{});
         for (int i = 0; i < list.size(); i++) {
@@ -97,7 +106,7 @@ public class Usuario {
             Usuario u = new Usuario(
                     (long) row[0], 
                     (String) row[1], 
-                    (String) row[2], 
+                    (long) row[2], 
                     (String) row[3], 
                     (String) row[4],
                     (String) row[5]);
@@ -106,9 +115,14 @@ public class Usuario {
         return usuarios;
     }
     
-    public static void adicionarUsuario (String role, String name, String login, long passwordHash) throws Exception{
-        String SQL = "INSERT INTO USUARIO VALUES (default, ? , ? , ? , ? )";
-        Object parameters[] = {role, name, login, passwordHash};
+    public static void adicionarUsuario (String nome, String login, long senha, String curso, String permissao) throws Exception{
+        String SQL = "INSERT INTO TB_USUARIO VALUES (default, ?, ?, ?," +
+                        "(SELECT CD_CURSO FROM TB_CURSO" +
+                            "WHERE NM_CURSO = ?)," +
+                        "(SELECT CD_PERMISSAO FROM TB_PERMISSAO" +
+                            "WHERE NM_PERMISSAO = ?)" +
+                      ");";
+        Object parameters[] = {nome, login, senha, curso, permissao};
         DatabaseConnector.execute(SQL, parameters);
     }
     
