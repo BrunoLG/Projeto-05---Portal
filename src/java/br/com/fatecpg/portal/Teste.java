63f483dfea0aa67fc6890269777b41d6f28f6e96
DESKTOP-IUO2BJA\Leona <leonardosantos1887@hotmail.com>
2018-12-05 00:38:58 -0200

    Classe teste estruturada

--- src/java/br/com/fatecpg/portal/Teste.java
+++ src/java/br/com/fatecpg/portal/Teste.java
@@ -1,5 +1,38 @@
 package br.com.fatecpg.portal;
 
 public class Teste {
+    private long cod;
+    private String nome;
+    private String disciplina;
+
+    public Teste(long cod, String nome, String disciplina) {
+        this.cod = cod;
+        this.nome = nome;
+        this.disciplina = disciplina;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public void setCod(long cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     
 }
