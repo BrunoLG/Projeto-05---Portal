a285036dd58d3539f8c4bdb440ad0bfc2ba023ea
DESKTOP-IUO2BJA\Leona <leonardosantos1887@hotmail.com>
2018-12-06 20:57:23 -0200

    Alterações na classe Questao

--- src/java/br/com/fatecpg/portal/Questao.java
+++ src/java/br/com/fatecpg/portal/Questao.java
@@ -48,6 +48,23 @@ public class Questao {
         this.codTeste = codTeste;
     }
     
+    public static ArrayList<Questao> getQuestoes() throws Exception {
+        String SQL = "SELECT CD_QUESTAO, DS_QUESTAO, NM_RESPOSTA, CD_TESTE" +
+                    " FROM TB_QUESTAO ";
+        ArrayList<Questao> questoes = new ArrayList<>();
+        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{});
+        for (int i = 0; i < list.size(); i++) {
+            Object row[] = list.get(i);
+            Questao q = new Questao(
+                    (long) row[0],
+                    (String) row[1],
+                    (String) row[2],
+                    (long) row[3]);
+            questoes.add(q);
+        }
+        return questoes;
+    }
+    
     public static ArrayList<Questao> getQuestoes(long cod) throws Exception {
         String SQL = "SELECT CD_QUESTAO, DS_QUESTAO, NM_RESPOSTA, CD_TESTE" +
                     " FROM TB_QUESTAO WHERE CD_TESTE = ? ";
@@ -67,10 +84,7 @@ public class Questao {
     }
     
     public static void adicionarQuestao (String enunciado, String resposta, String teste) throws Exception{
-        String SQL = "INSERT INTO TB_QUESTAO VALUES (default, ?, ? " +
-                        " (SELECT CD_TESTE FROM TB_TESTE " +
-                            "WHERE NM_TESTE = ? ) " +
-                      ")";
+        String SQL = "INSERT INTO TB_QUESTAO VALUES (default, ?, ?, ? ) ";
         Object parameters[] = {enunciado, resposta, teste};
         DatabaseConnector.execute(SQL, parameters);
     }
