8a58d273dbdbe62f5e472c6dd3b7be20544696e9
DESKTOP-IUO2BJA\Leona <leonardosantos1887@hotmail.com>
2018-12-06 21:00:23 -0200

    Alterações nas classes Alternativa e Teste

--- src/java/br/com/fatecpg/portal/Teste.java
+++ src/java/br/com/fatecpg/portal/Teste.java
@@ -52,12 +52,26 @@ public class Teste {
         }
         return testes;
     }
+
+    public static ArrayList<Teste> getTestes(String cod) throws Exception {
+        String SQL = "SELECT CD_TESTE, NM_TESTE, T.CD_DISCIPLINA " +
+            " FROM TB_TESTE T JOIN TB_DISCIPLINA D ON T.CD_DISCIPLINA = D.CD_DISCIPLINA " +
+            " WHERE D.CD_CURSO = (SELECT C.CD_CURSO FROM TB_CURSO C WHERE C.NM_CURSO = ?) ";
+        ArrayList<Teste> testes = new ArrayList<>();
+        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{cod});
+        for (int i = 0; i < list.size(); i++) {
+            Object row[] = list.get(i);
+            Teste t = new Teste(
+                    (long) row[0],
+                    (String) row[1], 
+                    (long) row[2]);
+            testes.add(t);
+        }
+        return testes;
+    }
     
-    public static void adicionarTeste (String nome, String disciplina) throws Exception{
-        String SQL = "INSERT INTO TB_TESTE VALUES (default, ? " +
-                        " (SELECT CD_DISCIPLINA FROM TB_DISCIPLINA " +
-                            "WHERE NM_DISCIPLINA = ? ) " +
-                      ")";
+    public static void adicionarTeste (String nome, long disciplina) throws Exception{
+        String SQL = "INSERT INTO TB_TESTE VALUES (default, ?, ?)";
         Object parameters[] = {nome, disciplina};
         DatabaseConnector.execute(SQL, parameters);
     }
