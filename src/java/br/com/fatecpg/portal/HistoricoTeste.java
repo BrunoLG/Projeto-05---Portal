ddc24f1c9441b05cc7b5ce641e2bdbd2b9bf14a1
DESKTOP-IUO2BJA\Leona <leonardosantos1887@hotmail.com>
2018-12-06 02:16:43 -0200

    Alterações nas classes HistoricoTeste e Teste

--- src/java/br/com/fatecpg/portal/HistoricoTeste.java
+++ src/java/br/com/fatecpg/portal/HistoricoTeste.java
@@ -9,13 +9,15 @@ public class HistoricoTeste {
     private Date data;
     private String curso;
     private String teste;
+    private String disciplina;
 
-    public HistoricoTeste(long cod, double nota, Date data, String curso, String teste) {
+    public HistoricoTeste(long cod, double nota, Date data, String curso, String teste, String disciplina) {
         this.cod = cod;
         this.nota = nota;
         this.data = data;
         this.curso = curso;
         this.teste = teste;
+        this.disciplina = disciplina;
     }
     
     public long getCod() {
@@ -58,16 +60,23 @@ public class HistoricoTeste {
         this.teste = teste;
     }
     
-    public static ArrayList<HistoricoTeste> getTesteHistorico() throws Exception {
-        String SQL = "SELECT CD_HISTORICO, CD_NOTA, DT_TESTE, " +
-            "(SELECT C.NM_CURSO FROM TB_USUARIO U " +
-            "JOIN TB_CURSO C ON U.CD_CURSO = C.CD_CURSO " +
-            "WHERE H.CD_USUARIO = U.CD_USUARIO), " +
-            "(SELECT U.NM_USUARIO FROM TB_USUARIO U " +
-            "WHERE U.CD_USUARIO = H.CD_USUARIO) " +
-            "FROM TB_HISTORICO_TESTE H ORDER BY CD_NOTA DESC";
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    
+    public static ArrayList<HistoricoTeste> getTesteHistorico(String curso) throws Exception {
+        String SQL = "SELECT CD_HISTORICO, CD_NOTA, DT_TESTE, NM_CURSO, NM_USUARIO, NM_DISCIPLINA  FROM TB_HISTORICO_TESTE H " +
+            " JOIN TB_TESTE T ON H.CD_TESTE = T.CD_TESTE " +
+            " JOIN TB_DISCIPLINA D ON T.CD_DISCIPLINA = D.CD_DISCIPLINA " +
+            " JOIN TB_USUARIO U ON U.CD_USUARIO = H.CD_USUARIO " +
+            " JOIN TB_CURSO C ON U.CD_CURSO = C.CD_CURSO " +
+            " WHERE NM_CURSO = ? ORDER BY CD_NOTA DESC";
         ArrayList<HistoricoTeste> historicoTestes = new ArrayList<>();
-        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{});
+        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{curso});
         for (int i = 0; i < list.size(); i++) {
             Object row[] = list.get(i);
             HistoricoTeste ht = new HistoricoTeste(
@@ -75,15 +84,24 @@ public class HistoricoTeste {
                     (Double) row[1],
                     (Date) row[2], 
                     (String) row[3],
-                    (String) row[4]);
+                    (String) row[4],
+                    (String) row[5]);
             historicoTestes.add(ht);
         }
         return historicoTestes;
     }
+    
+    public static void adicionarTesteHistorico (double nota, Date dataAtual, long teste, long usuario) throws Exception{
+        String SQL = "INSERT INTO TB_HISTORICO_TESTE VALUES (default, ?, ?, ?, ?) ";
+        Object parameters[] = {nota, dataAtual, teste, usuario};
+        DatabaseConnector.execute(SQL, parameters);
+    }
+
+
     public static ArrayList<HistoricoTeste> listarTestes (long cod)throws Exception{
-        String SQL = "select cd_historico, cd_nota, dt_teste, nm_disciplina, nm_usuario from tb_historico_teste H " +
+        String SQL = "select cd_historico, cd_nota, dt_teste, nm_curso, nm_usuario, nm_disciplina from tb_historico_teste H " +
                 "join tb_teste T on H.cd_teste = T.cd_teste join tb_disciplina D "
-                + " on T.cd_disciplina = D.cd_disciplina join tb_usuario U on U.cd_usuario = H.cd_usuario where H.cd_usuario = ?";
+                + " on T.cd_disciplina = D.cd_disciplina join tb_usuario U on U.cd_usuario = H.cd_usuario join tb_curso C on U.cd_curso = C.cd_curso where H.cd_usuario = ?";
         ArrayList<HistoricoTeste> testes = new ArrayList<>();
         ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{cod});
         for (int i = 0; i < list.size(); i++) {
@@ -93,7 +111,8 @@ public class HistoricoTeste {
                     (double) row[1],
                     (Date) row[2],
                     (String) row[3],
-                    (String) row[4]);
+                    (String) row[4],
+                    (String) row[5]);
             testes.add(t);
         }
         return testes;
--- src/java/br/com/fatecpg/portal/Teste.java
+++ src/java/br/com/fatecpg/portal/Teste.java
@@ -5,19 +5,19 @@ import java.util.ArrayList;
 public class Teste {
     private long cod;
     private String nome;
-    private String disciplina;
-
-    public Teste(long cod, String nome, String disciplina) {
+    private long disciplina;
+    
+    public Teste(long cod, String nome, long disciplina) {
         this.cod = cod;
         this.nome = nome;
         this.disciplina = disciplina;
     }
 
-    public String getDisciplina() {
+    public long getDisciplina() {
         return disciplina;
     }
 
-    public void setDisciplina(String disciplina) {
+    public void setDisciplina(long disciplina) {
         this.disciplina = disciplina;
     }
 
@@ -35,5 +35,30 @@ public class Teste {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }  
+    }
+    
+    public static ArrayList<Teste> getTeste(long cod) throws Exception {
+        String SQL = "SELECT CD_TESTE, NM_TESTE, CD_DISCIPLINA" +
+                    " FROM TB_TESTE WHERE CD_DISCIPLINA = ? ";
+        ArrayList<Teste> testes = new ArrayList<>();
+        ArrayList<Object[]> list = DatabaseConnector.getQuery(SQL, new Object[]{cod});
+        for (int i = 0; i < list.size(); i++) {
+            Object row[] = list.get(i);
+            Teste t = new Teste(
+                    (long) row[0],
+                    (String) row[1], 
+                    (long) row[2]);
+            testes.add(t);
+        }
+        return testes;
+    }
+    
+    public static void adicionarTeste (String nome, String disciplina) throws Exception{
+        String SQL = "INSERT INTO TB_TESTE VALUES (default, ? " +
+                        " (SELECT CD_DISCIPLINA FROM TB_DISCIPLINA " +
+                            "WHERE NM_DISCIPLINA = ? ) " +
+                      ")";
+        Object parameters[] = {nome, disciplina};
+        DatabaseConnector.execute(SQL, parameters);
+    }
 }
